2394c980d6a082762634e444bb191ddc8b83c56f
Vilius Albrechtas <41568638+wolfo951@users.noreply.github.com>
2025-03-13 08:59:53 +0200

    Fixing `imodel connection create` command and it's documentation (#28)

--- src/commands/imodel/connection/create.ts
+++ src/commands/imodel/connection/create.ts
@@ -8,15 +8,51 @@ import { Flags } from "@oclif/core";
 import BaseCommand from "../../../extensions/base-command.js";
 import { authenticationType } from "../../../services/synchronizationClient/models/authentication-type.js";
 import { connectorType } from "../../../services/synchronizationClient/models/connector-type.js";
+import { storageFileCreate } from "../../../services/synchronizationClient/models/storage-file-create.js";
 
 export default class CreateConnection extends BaseCommand {
   static description = 'Create a new connector.';
 
   static flags = {
-    "authentication-type": Flags.string({ description: 'The authorization workflow type.', options: ['User', 'Service'], required: false }),
-    "connector-type": Flags.string({ description: 'The connector type of your file.', required: true }),
-    "file-id": Flags.string({ description: 'The file id.', required: true }),
-    "imodel-id": Flags.string({ description: 'The id of the iModel.', required: true }),
+    "authentication-type": Flags.string({ 
+      description: 'The authorization workflow type.', 
+      options: ['User', 'Service'], 
+      required: false 
+    }),
+    "connector-type": Flags.string({ 
+      description: 'The connector type of your file. Each connector will be used for the corresponding file in the files list (first connector for the first file, second connector for the second file, and so on).', 
+      multiple: true,
+      options: [
+        'AUTOPLANT',
+        'AVEVAPID',
+        'CIVIL',
+        'CIVIL3D',
+        'DWG',
+        'GEOSPATIAL',
+        'IFC',
+        'MSTN',
+        'NWD',
+        'OBD',
+        'OPENTOWER',
+        'REVIT',
+        'SPPID',
+        'SPXREVIEW' 
+      ],
+      required: true
+    }),
+    "display-name": Flags.string({
+      description: 'The display name of the storage connection.',
+      required: false,
+    }),
+    "file-id": Flags.string({ 
+      description: 'The ID of the storage file to synchronize', 
+      multiple: true,
+      required: true
+    }),
+    "imodel-id": Flags.string({ 
+      description: 'The ID of the iModel.', 
+      required: true 
+    }),
   };
 
   async run() {
@@ -24,13 +60,23 @@ export default class CreateConnection extends BaseCommand {
 
     const client = await this.getSynchronizationClient();
 
+    const sourceFiles : storageFileCreate[] = [];
+
+    if(flags["connector-type"].length !== flags["file-id"].length) {
+      this.error("The number of connector types must match the number of file ids.");
+    }
+
+    for (let i = 0; i < flags["file-id"].length; i++) {
+      sourceFiles.push({
+        connectorType: flags["connector-type"][i] as connectorType,
+        storageFileId: flags["file-id"][i]
+      });
+    }
+
     const response = await client.createStorageConnection({
       authenticationType: flags["authentication-type"] as authenticationType,
       iModelId: flags["imodel-id"],
-      sourceFiles: [{
-        connectorType: flags["connector-type"] as connectorType,
-        storageFileId: flags["file-id"],
-      }]
+      sourceFiles
     });
 
     return this.logAndReturnResult(response.connection);
