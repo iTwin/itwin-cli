237a1603eee1d6ca930ec74eecbe55655fb18330
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-24 13:57:33 +0300

    Added documentation for is-main-module.ts

--- integration-tests/utils/is-main-module.ts
+++ integration-tests/utils/is-main-module.ts
@@ -6,6 +6,12 @@
 import process from 'node:process';
 import {fileURLToPath} from 'node:url';
 
+/**
+ * Checks whether the current test file being executed matches the file/pattern, that was provided as an argument to mocha.
+ * This makes it possible to both export tests and be able to run only part of the tests with 'Debug Active Test' launch configuration.
+ * @param meta `import.meta` object of the current file.
+ * @returns `true`, if the current file is not an import, otherwise `false`
+ */
 export default function isMainModule(meta: {url: string}) {
     if (!meta || !process.argv[2]) {
         return false;
