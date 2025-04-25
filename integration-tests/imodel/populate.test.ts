9f2e78951506d94b4e9d3450a46f50e2c774130b
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-25 16:45:33 +0300

    Increased timeout for imodel populate test. Updated after() function in imodel populate tests to prevent cleanup failures.

--- integration-tests/imodel/populate.test.ts
+++ integration-tests/imodel/populate.test.ts
@@ -6,7 +6,7 @@
 import { runCommand } from '@oclif/test';
 import { expect } from 'chai';
 
-import { createFile, createIModel, createITwin, deleteFile, deleteIModel, deleteITwin, getRootFolderId } from '../utils/helpers';
+import { createFile, createIModel, createITwin, getRootFolderId } from '../utils/helpers';
 import runSuiteIfMainModule from '../utils/run-suite-if-main-module';
 
 const tests = () => describe('populate', () => {
@@ -27,15 +27,19 @@ const tests = () => describe('populate', () => {
   });
 
   after(async () => {
-    await deleteFile(testFileId);
-    await deleteIModel(testIModelId);
-    await deleteITwin(testITwinId);
+    const fileDeleteResult = await runCommand(`storage file delete --file-id ${testFileId}`);
+    const iModelDeleteResult = await runCommand(`imodel delete --imodel-id ${testIModelId}`);
+    const iTwinDeleteResult = await runCommand(`itwin delete --itwin-id ${testITwinId}`);
+
+    expect(fileDeleteResult).to.have.property('result', 'deleted');
+    expect(iModelDeleteResult).to.have.property('result', 'deleted');
+    expect(iTwinDeleteResult).to.have.property('result', 'deleted');
   });
 
   it('should populate the iModel with the uploaded file', async () => {
     const result = await runCommand(`imodel populate --imodel-id ${testIModelId} --file ${testFilePath} --connector-type SPPID`);
     expect(result.result).to.have.property('iModelId', testIModelId);
-  }).timeout(20 * 60 * 1000);
+  }).timeout(30 * 60 * 1000);
 });
 
 export default tests;
