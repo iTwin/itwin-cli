339e065946c7fc06c9c03874b525ee3716deb229
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-29 15:11:03 +0300

    Added some debugging code to powershell scripts.

--- integration-tests/main-cases/native-client.ps1
+++ integration-tests/main-cases/native-client.ps1
@@ -1,5 +1,7 @@
 Set-Location "$PSScriptRoot/../.."
 
+Get-Location
+
 ./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/native-client-serial.test.ts
 
 ./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/native-client-parallel/*.test.ts
--- integration-tests/main-cases/service-client.ps1
+++ integration-tests/main-cases/service-client.ps1
@@ -1,5 +1,7 @@
 Set-Location "$PSScriptRoot/../.."
 
+Get-Location
+
 ./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/service-client-serial.test.ts
 
 ./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/service-client-parallel/*.test.ts
