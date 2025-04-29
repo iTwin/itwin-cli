ba9d900dd438abe665fec2f7c110e34a84440b4e
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-29 16:42:07 +0300

    Added amount of jobs to parallel mocha calls.

--- integration-tests/main-cases/native-client.ps1
+++ integration-tests/main-cases/native-client.ps1
@@ -1,4 +1,4 @@
 Set-Location "$PSScriptRoot/../.."
 
 npx mocha --forbid-only integration-tests/main-cases/native-client-serial.test.ts
-npx mocha --forbid-only --parallel integration-tests/main-cases/native-client-parallel/*.test.ts
+npx mocha --forbid-only --parallel --jobs 4 integration-tests/main-cases/native-client-parallel/*.test.ts
--- integration-tests/main-cases/service-client.ps1
+++ integration-tests/main-cases/service-client.ps1
@@ -1,4 +1,4 @@
 Set-Location "$PSScriptRoot/../.."
 
 npx mocha --forbid-only integration-tests/main-cases/service-client-serial.test.ts
-npx mocha --forbid-only --parallel integration-tests/main-cases/service-client-parallel/*.test.ts
+npx mocha --forbid-only --parallel --jobs 4 integration-tests/main-cases/service-client-parallel/*.test.ts
