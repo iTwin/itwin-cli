14bc52596f2939f93624292ff20be3368014d5e7
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-29 15:26:32 +0300

    Updated powershell scripts.

--- integration-tests/main-cases/native-client.ps1
+++ integration-tests/main-cases/native-client.ps1
@@ -1,5 +1,3 @@
-Set-Location "$PSScriptRoot/../.."
+../../node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/native-client-serial.test.ts
 
-./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/native-client-serial.test.ts
-
-./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/native-client-parallel/*.test.ts
+../../node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/native-client-parallel/*.test.ts
--- integration-tests/main-cases/service-client.ps1
+++ integration-tests/main-cases/service-client.ps1
@@ -1,5 +1,3 @@
-Set-Location "$PSScriptRoot/../.."
+../../node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/service-client-serial.test.ts
 
-./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/service-client-serial.test.ts
-
-./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/service-client-parallel/*.test.ts
+../../node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/service-client-parallel/*.test.ts
