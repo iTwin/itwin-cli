620e47299c8e3e20f9738affd06efbaced62fdb2
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-04-29 15:19:47 +0300

    Replaced powershell action with shell action.

--- integration-tests/main-cases/native-client.ps1
+++ integration-tests/main-cases/native-client.ps1
@@ -1,7 +1,5 @@
 Set-Location "$PSScriptRoot/../.."
 
-Get-Location
-
 ./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/native-client-serial.test.ts
 
 ./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/native-client-parallel/*.test.ts
--- integration-tests/main-cases/service-client.ps1
+++ integration-tests/main-cases/service-client.ps1
@@ -1,7 +1,5 @@
 Set-Location "$PSScriptRoot/../.."
 
-Get-Location
-
 ./node_modules/.bin/mocha.ps1 --forbid-only integration-tests/main-cases/service-client-serial.test.ts
 
 ./node_modules/.bin/mocha.ps1 --forbid-only --parallel integration-tests/main-cases/service-client-parallel/*.test.ts
