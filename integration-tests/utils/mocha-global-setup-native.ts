8c0cd4a1dde3e0d91f62bae64e7ffc09ccabe7cc
Linas Burneika <linas.burneika@bentley.com>
2025-07-04 17:32:55 +0300

    temporary removing imodel connection auth from global test setup

--- integration-tests/utils/mocha-global-setup-native.ts
+++ integration-tests/utils/mocha-global-setup-native.ts
@@ -3,17 +3,10 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { expect } from "chai";
-
-import { runCommand } from "@oclif/test";
-
-import { AuthInfo } from "../../src/services/synchronizationClient/models/connection-auth";
 import { nativeLoginToCli } from "./helpers";
 
 export async function mochaGlobalSetup(): Promise<void> {
   await nativeLoginToCli();
-  const { result } = await runCommand<AuthInfo>(`imodel connection auth`);
-  expect(result?.isUserAuthorized).to.be.equal(true);
 
   // eslint-disable-next-line no-console
   console.log("\n\nRunning tests with native client");
