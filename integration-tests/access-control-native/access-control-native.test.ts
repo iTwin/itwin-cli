a632db4eeb1aba889e913328c56d45684795b29b
Karolis Zukauskas <Karoliss.Zukauskas@bentley.com>
2025-08-01 17:11:42 +0300

    Fixed integration tests.

--- integration-tests/access-control-native/access-control-native.test.ts
+++ integration-tests/access-control-native/access-control-native.test.ts
@@ -5,12 +5,10 @@
 
 import runSuiteIfMainModule from "../utils/run-suite-if-main-module";
 import groupTests from "./group.test";
-import memberTests from "./member/member.test";
 
 const tests = () =>
   describe("Access Control Tests (Native Client)", () => {
     groupTests();
-    memberTests();
   });
 
 export default tests;
